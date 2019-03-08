50f59c6924decc13fc7968361236de5dc2677d7d
Zheng Wei <wzweizheng@qq.com>
2019-03-08 15:16:33 -0800

    add test files and remove genome configure function into pipeFrame

--- R/untils.R
+++ R/untils.R
@@ -1,35 +1,10 @@
+#' @importFrom R.utils gunzip
+#' @importFrom pipeFrame getRefRc
+#' @importFrom pipeFrame checkAndInstallBSgenome
+#' @importFrom pipeFrame checkAndInstallGenomeFa
+#' @importFrom pipeFrame getRefFiles
+#' @importFrom BSgenome getBSgenome
 
-checkAndInstallBSgenome <- function(resultDirPaths){
-    genome <- getGenome()
-    bsgenomename<- BSgenome::available.genomes()[grepl(paste0(genome,"$"),BSgenome::available.genomes())]
-    if(length(bsgenomename)==0){
-        message()
-        stop("There is no BSgenome support for this genome")
-    }
-    bsgenomeinstall <- BSgenome::installed.genomes()[grepl(paste0(genome,"$"),BSgenome::installed.genomes())]
-    if(length(bsgenomeinstall)==0){
-        message(paste("BSgenome for ",genome,"has not been installed,"))
-        message("begin to install ...")
-        BiocManager::install(bsgenomename)
-    }
-}
-
-checkAndInstallGenomeFa <- function(resultDirPaths){
-    outFile <- resultDirPaths
-    bsgenome<-getRefRs("bsgenome")
-    if(!is(bsgenome, "BSgenome")){stop("The variable 'bsgenome' is not a BSgenome")}
-    append <- FALSE
-    for(chrT in seqnames(bsgenome)){
-        if(is.null(masks(bsgenome[[chrT]])))
-            chrSeq <- DNAStringSet(bsgenome[[chrT]])
-        else
-            chrSeq <- DNAStringSet(injectHardMask(bsgenome[[chrT]], letter="N"))
-        names(chrSeq) <- chrT
-        writeXStringSet(chrSeq, filepath=outFile, format="fasta", append=append)
-        append <- TRUE
-    }
-    return(outFile)
-}
 
 dowloadMotifFile <- function(resultDirPaths){
     download.file(url = "https://wzthu.github.io/enrich/refdata/all_motif_rmdup",
@@ -60,19 +35,16 @@ convertPWMFileToPWMobj <- function(resultDirPaths){
 
 
 dowloadMotifTFTableFile <- function(resultDirPaths){
-    genome <-getGenome()
     download.file(url = "https://wzthu.github.io/enrich/refdata/MotifTFTable.RData",
                   destfile = resultDirPaths,method = getOption("download.file.method"))
 
 }
 dowloadMotifWeightsFile <- function(resultDirPaths){
-    genome <-getGenome()
     download.file(url = "https://wzthu.github.io/enrich/refdata/MotifWeights.RData",
                   destfile = resultDirPaths,method = getOption("download.file.method"))
 
 }
 dowloadTFgeneRelMtxFile <- function(resultDirPaths){
-    genome <-getGenome()
     download.file(url = "https://wzthu.github.io/enrich/refdata/TFgeneRelMtx.RData",
                   destfile = resultDirPaths,method = getOption("download.file.method"))
 
